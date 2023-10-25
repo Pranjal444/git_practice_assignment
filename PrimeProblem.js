aa335225c490b09818999024e05a9c25edd7390c
Priyanshud16 <priyanshuharrdy@gmail.com>
2023-10-25 19:32:38 +0530

    Fixed Prime Problem (Member 2)

--- PrimeProblem.js
+++ PrimeProblem.js
@@ -1,19 +1,6 @@
-function isPrime(number) {
-    if (number <= 1) {
-      return false;
-    }
-    if (number <= 3) {
-      return true;
-    }
-    if (number % 2 === 0 || number % 3 === 0) {
-      return false;
-    }
-  
-    for (let i = 5; i * i <= number; i += 6) {
-      if (number % i === 0 || number % (i + 2) === 0) {
-        return false;
-      }
-    }
-  
-    return true;
-  }
+const num = 17; // Replace with the number you want to check
+if (isPrime(num)) {
+  console.log(num + " is a prime number.");
+} else {
+  console.log(num + " is not a prime number.");
+}
